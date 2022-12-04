0b7fff569624844cc5d1d62718cb942c9bd4fba7
tiaozi <1184824553@qq.com>
2022-12-04 15:32:47 +0800

    5646

---
+++ app/src/main/java/com/dream/xiaobei/user/ui/RatingStarsActivity.kt
@@ -0,0 +1,19 @@
+package com.dream.xiaobei.user.ui
+
+import android.os.Bundle
+import com.dream.xiaobei.databinding.ActivityRatingStarsBinding
+import com.dream.xiaobei.user.vm.UserViewModel
+import com.tcl.base.common.ui.BaseActivity
+
+class RatingStarsActivity : BaseActivity<UserViewModel,ActivityRatingStarsBinding>() {
+    override fun initView(savedInstanceState: Bundle?) {
+
+    }
+
+    override fun initData() {
+
+    }
+
+    override fun initDataOnResume() {
+    }
+}
